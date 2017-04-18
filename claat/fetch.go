didrocks/codelab-ubuntu-tools

Enable local relative path copy of images.
As all images were imported from gdoc (and so remote), the case of local images
from markdown files weren't handled.
Ensure we can now fetch images remotely (from gdoc, drive or remote reference
from a markdown file) or locally. We compute relative path from the source
file itself for local images and reuse it when running "update".

claat/fetch.go
@@ -29,6 +29,8 @@ import (
 	"strings"
 	"time"
 
+	"path"
+
 	"github.com/didrocks/codelab-ubuntu-tools/claat/parser"
 	"github.com/didrocks/codelab-ubuntu-tools/claat/types"
 )
@@ -227,20 +229,40 @@ func fetchDriveFile(id string, nometa bool) (*resource, error) {
 
 var crcTable = crc64.MakeTable(crc64.ECMA)
 
-func slurpBytes(client *http.Client, dir, url string, n int) (string, error) {
-	res, err := retryGet(client, url, n)
+func slurpBytes(client *http.Client, codelabSrc, dir, imgURL string, n int) (string, error) {
+	// images can be local in Markdown cases or remote.
+	// Only proceed a simple copy on local reference.
+	var b []byte
+	var ext string
+	u, err := url.Parse(imgURL)
 	if err != nil {
 		return "", err
 	}
-	defer res.Body.Close()
-	b, err := ioutil.ReadAll(res.Body)
+	if u.Host == "" {
+		imgURL = path.Join(path.Dir(codelabSrc), imgURL)
+		b, err = ioutil.ReadFile(imgURL)
+		ext = path.Ext(imgURL)
+	} else {
+		b, err = slurpRemoteBytes(client, dir, imgURL, 5)
+		ext = ".png"
+	}
 	if err != nil {
 		return "", err
 	}
+
 	crc := crc64.Checksum(b, crcTable)
-	file := fmt.Sprintf("%x.png", crc)
+	file := fmt.Sprintf("%x%s", crc, ext)
 	dst := filepath.Join(dir, file)
 	return file, ioutil.WriteFile(dst, b, 0644)
+}
+
+func slurpRemoteBytes(client *http.Client, dir, url string, n int) ([]byte, error) {
+	res, err := retryGet(client, url, n)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	return ioutil.ReadAll(res.Body)
 }
 
 // retryGet tries to GET specified url up to n times.

googlecodelabs/tools

As every images were imported from gdoc (and so, a remote destination),
the case of local images from markdown files wasn't handled.
We force and ensure that assets are located inside parent codelab directory
to prevent potential sensitive system info leaks.
Add some manual and quick fuzz testing.

claat/fetch.go
@@ -227,20 +227,42 @@ func fetchDriveFile(id string, nometa bool) (*resource, error) {
 
 var crcTable = crc64.MakeTable(crc64.ECMA)
 
-func slurpBytes(client *http.Client, dir, url string, n int) (string, error) {
-	res, err := retryGet(client, url, n)
+func slurpBytes(client *http.Client, codelabSrc, dir, imgURL string, n int) (string, error) {
+	// images can be local in Markdown cases or remote.
+	// Only proceed a simple copy on local reference.
+	var b []byte
+	var ext string
+	u, err := url.Parse(imgURL)
 	if err != nil {
 		return "", err
 	}
-	defer res.Body.Close()
-	b, err := ioutil.ReadAll(res.Body)
+	if u.Host == "" {
+		if imgURL, err = restrictPathToParent(imgURL, filepath.Dir(codelabSrc)); err != nil {
+			return "", err
+		}
+		b, err = ioutil.ReadFile(imgURL)
+		ext = filepath.Ext(imgURL)
+	} else {
+		b, err = slurpRemoteBytes(client, dir, imgURL, 5)
+		ext = ".png"
+	}
 	if err != nil {
 		return "", err
 	}
+
 	crc := crc64.Checksum(b, crcTable)
-	file := fmt.Sprintf("%x.png", crc)
+	file := fmt.Sprintf("%x%s", crc, ext)
 	dst := filepath.Join(dir, file)
 	return file, ioutil.WriteFile(dst, b, 0644)
+}
+
+func slurpRemoteBytes(client *http.Client, dir, url string, n int) ([]byte, error) {
+	res, err := retryGet(client, url, n)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	return ioutil.ReadAll(res.Body)
 }
 
 // retryGet tries to GET specified url up to n times.
@@ -305,4 +327,20 @@ func gdocID(url string) string {
 
 func gdocExportURL(id string) string {
 	return fmt.Sprintf("%s/files/%s/export?mimeType=text/html", driveAPI, id)
+}
+
+// restrictPathToParent will ensure that assetPath is in parent.
+// It will thus return an absolute path to the asset.
+func restrictPathToParent(assetPath, parent string) (string, error) {
+	parent, err := filepath.Abs(parent)
+	if err != nil {
+		return "", err
+	}
+	if !filepath.IsAbs(assetPath) {
+		assetPath = filepath.Join(parent, assetPath)
+	}
+	if !strings.HasPrefix(assetPath, parent) {
+		return "", fmt.Errorf("%s isn't a subdirectory of %s", assetPath, parent)
+	}
+	return assetPath, nil
 }