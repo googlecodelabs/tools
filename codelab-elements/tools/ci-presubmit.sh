36f60dfec4d49a2bc5f320f96ea7a962f8bdd685
Shawn Busolits <sbusolits@google.com>
2019-01-02 14:39:16 -0500

    Adding bazel build to the Travis presubmit.

--- codelab-elements/tools/ci-presubmit.sh
+++ codelab-elements/tools/ci-presubmit.sh
@@ -15,11 +15,10 @@
 
 set -ex
 
-use_bazel.sh 0.19.1
-
 # Make this repo's bazel workspace the current work dir.
 # Relative to this script location.
-cd $(dirname $0)/..
+cd $(dirname $0)/../..
+pwd
 bazel info
 
 BAZEL_FLAGS="--color=no \
