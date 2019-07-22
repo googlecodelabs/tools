619f3ad638641ec69aad374389cd56f138ccb0ec
Marcial1234 <marcial.abrahantes@gmail.com>
2019-07-22 14:27:51 -0400

    Formatted oneof_utils to use tabs instead of spaces

--- claat/proto/renderer/oneof_utils.go
+++ claat/proto/renderer/oneof_utils.go
@@ -14,51 +14,51 @@
 package genrenderer
 
 import (
-  "text/template"
+	"text/template"
 
-  "github.com/googlecodelabs/tools/third_party"
+	"github.com/googlecodelabs/tools/third_party"
 )
 
 // RenderOneof returns the underyling, rendered, passed oneof element
 func RenderOneof(el interface{}, t *template.Template) string {
-  // Recursive redering happens here
-  return ExecuteTemplate(typeAssertUnderlingOneofType(el), t)
+	// Recursive redering happens here
+	return ExecuteTemplate(typeAssertUnderlingOneofType(el), t)
 }
 
 // typeAssertUnderlingOneofType turns a generic oneof proto into its underlying typed-proto
 func typeAssertUnderlingOneofType(el interface{}) (underlyingType interface{}) {
-  // Pure Oneof protos
-  switch el.(type) {
-  case *tutorial.InlineContent:
-    underlyingType = underlyingInlineContentType(el.(*tutorial.InlineContent))
-  }
+	// Pure Oneof protos
+	switch el.(type) {
+	case *tutorial.InlineContent:
+		underlyingType = underlyingInlineContentType(el.(*tutorial.InlineContent))
+	}
 
-  // debug-friendly panic
-  if underlyingType == nil {
-    panic(TypeNotSupported("typeAssertUnderlingOneofType", el))
-  }
+	// debug-friendly panic
+	if underlyingType == nil {
+		panic(TypeNotSupported("typeAssertUnderlingOneofType", el))
+	}
 
-  return underlyingType
+	return underlyingType
 }
 
 // underlyingInlineContentType asserts the underlying type of tutorial.InlineContent
 func underlyingInlineContentType(el *tutorial.InlineContent) (underlyingType interface{}) {
-  switch x := el.Content.(type) {
-  case *tutorial.InlineContent_Text:
-    // StylizedText
-    underlyingType = x.Text
-  case *tutorial.InlineContent_Code:
-    // InlineCode
-    underlyingType = x.Code
-  case *tutorial.InlineContent_Link:
-    // Link
-    underlyingType = x.Link
-  }
+	switch x := el.Content.(type) {
+	case *tutorial.InlineContent_Text:
+		// StylizedText
+		underlyingType = x.Text
+	case *tutorial.InlineContent_Code:
+		// InlineCode
+		underlyingType = x.Code
+	case *tutorial.InlineContent_Link:
+		// Link
+		underlyingType = x.Link
+	}
 
-  // debug-friendly panic
-  if underlyingType == nil {
-    panic(TypeNotSupported("InnerInline", el))
-  }
+	// debug-friendly panic
+	if underlyingType == nil {
+		panic(TypeNotSupported("InnerInline", el))
+	}
 
-  return underlyingType
+	return underlyingType
 }
