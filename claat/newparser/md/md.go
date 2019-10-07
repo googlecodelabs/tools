f45d5176001caffc67c00435039f3261ecbbc7a2
Cassie Recher <cerech@google.com>
2019-10-07 12:28:06 -0700

    Add tests for Parse.

--- claat/newparser/md/md.go
+++ claat/newparser/md/md.go
@@ -14,19 +14,24 @@ import (
 	"gopkg.in/russross/blackfriday.v2"
 )
 
-type preludeParseMode int
+type parseMode int
 
 const (
-	normal preludeParseMode = iota
-	title
-	metadata
+	preludeBase parseMode = iota
+	codelabTitle
+	codelabMetadata
+
+	stepBase
+	stepTitle
 )
 
 type parser struct {
+	currentNode *html.Node
+
 	metadata map[string]string
 	codelab  *types.Codelab
 
-	currentPreludeMode preludeParseMode
+	mode parseMode
 }
 
 func newParser() *parser {
@@ -60,21 +65,10 @@ func Parse(in io.Reader) (*types.Codelab, map[string]string, error) {
 	}
 
 	p := newParser()
+	p.currentNode = bodyNode
 
-	// Parse the prelude to locate the first step.
-	step, err := p.parsePrelude(bodyNode)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Iterate over steps until there are none left.
-	for step != nil {
-		p.codelab.NewStep("")
-		step, err = p.parseStep(step)
-		if err != nil {
-			return nil, nil, err
-		}
-	}
+	// Start the recursion
+	p.processCurrentNode()
 
 	return p.codelab, p.metadata, nil
 }
@@ -96,55 +90,13 @@ func seek(in *html.Node, a atom.Atom) (*html.Node, error) {
 	return nil, fmt.Errorf("could not find a node with atom %q", a.String())
 }
 
-// parsePrelude expects an *html.Node that is the <body> of the processed Markdown.
-// It handles the prelude, which is all codelab content occurring prior to the first step.
-// This is done by preorder-traversing the tree, handling nodes as they are encountered, and stopping at the first <h2>.
-// If it finds the <h2>, it will return that <h2>'s node.
-// If it encounters an error before finding the <h2>, it will return an error.
-// If no <h2> or error is found, it will return nil for both values.
-func (p *parser) parsePrelude(node *html.Node) (*html.Node, error) {
-	// Test for errors.
-	if node.Type == html.ErrorNode {
-		return nil, errors.New("encountered an error node while parsing")
+func (p *parser) inPrelude() bool {
+	switch p.mode {
+	case preludeBase, codelabTitle, codelabMetadata:
+		return true
+	default:
+		return false
 	}
-
-	// Handle the node based on the current mode.
-	switch p.currentPreludeMode {
-	case title:
-		// We're handling the codelab title. Pass text along and ignore everything else.
-		if node.Type == html.TextNode {
-			p.codelab.Title += node.Data
-		}
-	case metadata:
-		p.metadata = merge(p.metadata, extractMetadata(node.Data))
-		break
-	case normal:
-		switch node.DataAtom {
-		case atom.H1:
-			p.currentPreludeMode = title
-		case atom.P:
-			p.currentPreludeMode = metadata
-		case atom.H2:
-			// The start of the first step.
-			return node, nil
-		}
-	}
-
-	// The node itself has been handled; now we handle the child nodes recursively.
-	for next := node.FirstChild; next != nil; next = next.NextSibling {
-		result, err := p.parsePrelude(next)
-		if err != nil || result != nil {
-			return result, err
-		}
-	}
-
-	// Exit prelude modes.
-	if p.currentPreludeMode == title && node.DataAtom == atom.H1 || p.currentPreludeMode == metadata && node.DataAtom == atom.P {
-		p.currentPreludeMode = normal
-	}
-
-	// Nothing found.
-	return nil, nil
 }
 
 // Merges two string:string maps.
@@ -159,6 +111,86 @@ func merge(m1 map[string]string, m2 map[string]string) map[string]string {
 		m[k] = v
 	}
 	return m
+}
+
+func (p *parser) processCurrentNode() error {
+	fmt.Printf("%+v\n", p.currentNode.DataAtom)
+	// Test for errors.
+	if p.currentNode.Type == html.ErrorNode {
+		return errors.New("encountered an error node while parsing")
+	}
+
+	var popAction func()
+	if p.inPrelude() {
+		popAction = p.processPreludeNode()
+	} else {
+		popAction = p.processStepNode()
+	}
+
+	for next := p.currentNode.FirstChild; next != nil; next = next.NextSibling {
+		p.currentNode = next
+		p.processCurrentNode()
+	}
+
+	if popAction != nil {
+		popAction()
+	}
+
+	return nil
+}
+
+func (p *parser) processPreludeNode() func() {
+	var popAction func()
+	// Handle the node based on the current mode.
+	switch p.mode {
+	case codelabTitle:
+		// We're handling the codelab title. Pass text along and ignore everything else.
+		if p.currentNode.Type == html.TextNode {
+			p.codelab.Title += p.currentNode.Data
+		}
+	case codelabMetadata:
+		p.metadata = merge(p.metadata, extractMetadata(p.currentNode.Data))
+		break
+	case preludeBase:
+		switch p.currentNode.DataAtom {
+		case atom.H1:
+			p.mode = codelabTitle
+			popAction = func() {
+				p.mode = preludeBase
+			}
+		case atom.P:
+			p.mode = codelabMetadata
+			popAction = func() {
+				p.mode = preludeBase
+			}
+		case atom.H2:
+			// The start of the first step.
+			popAction = p.processStepNode()
+		}
+	}
+
+	// Nothing found.
+	return popAction
+}
+
+func (p *parser) processStepNode() func() {
+	// TODO actually handle step content
+	var popAction func()
+	switch p.mode {
+	case stepTitle:
+		if p.currentNode.Type == html.TextNode {
+			p.codelab.Steps[len(p.codelab.Steps)-1].Title += p.currentNode.Data
+		}
+	}
+
+	if p.currentNode.DataAtom == atom.H2 {
+		p.codelab.NewStep("")
+		p.mode = stepTitle
+		popAction = func() {
+			p.mode = stepBase
+		}
+	}
+	return popAction
 }
 
 // extractMetadata extracts metadata key-value pairs from string input.
@@ -177,30 +209,4 @@ func extractMetadata(s string) map[string]string {
 		}
 	}
 	return meta
-}
-
-// parseStep expects an *html.Node that is an <h2> in the processed Markdown.
-// It handles the step defined by the given node, up to the beginning of the next step, or the end of the content.
-// This is done by the preorder-traversing the tree, handling nodes as they are encountered, and stopping at the next <h2>, or at the end of input.
-// If another step comes after this one, this function will return the <h2> starting that step.
-// If the end of the content is found, it will return nil.
-// If it encounters an error before finding an <h2> or the end of the content, it will return an error.
-func (p *parser) parseStep(node *html.Node) (*html.Node, error) {
-
-	// TODO: actually handle step content
-
-	var nextStep *html.Node
-
-	// We don't really handle step content yet, so we can skip ahead.
-	for next := node.FirstChild; next != nil; next = next.NextSibling {
-		nextStep, err := seek(next, atom.H2)
-		if err != nil {
-			return nil, err
-		}
-		if nextStep != nil {
-			break
-		}
-	}
-
-	return nextStep, nil
 }
--- claat/newparser/md/md_test.go
+++ claat/newparser/md/md_test.go
@@ -2,6 +2,8 @@ package md
 
 import (
 	"bytes"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/googlecodelabs/tools/claat/types"
@@ -10,17 +12,19 @@ import (
 )
 
 func TestParse(t *testing.T) {
+	type output struct {
+		codelab  *types.Codelab
+		metadata map[string]string
+	}
 	tests := []struct {
-		in  string
-		out struct {
-			codelab  *types.Codelab
-			metadata map[string]string
-		}
-		ok bool
-	}{}
-}
-
-const in = `this is some text before the title
+		name string
+		in   string
+		out  output
+		ok   bool
+	}{
+		{
+			name: "Simple",
+			in: `this is some text before the title
 
 # Title Of Codelab
 
@@ -41,7 +45,67 @@ blah
 
 foo
 bar
-baz`
+baz`,
+			out: output{
+				codelab: &types.Codelab{
+					Meta: types.Meta{
+						Title: "Title Of Codelab",
+						Extra: map[string]string{},
+					},
+					Steps: []*types.Step{
+						&types.Step{
+							Title: "First Step",
+						},
+						&types.Step{
+							Title: "Second Step",
+						},
+					},
+				},
+				metadata: map[string]string{"original publication date": "2019-09-23", "events": "IO2019"},
+			},
+			ok: true,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			codelab, metadata, err := Parse(strings.NewReader(tc.in))
+
+			if err == nil && !tc.ok {
+				t.Errorf("Parse(string) = %+v, %+v, want err", codelab, metadata)
+				return
+			}
+			if err != nil && tc.ok {
+				t.Errorf("Parse(string) = %+v, want %+v, %+v", err, tc.out.codelab, tc.out.metadata)
+				return
+			}
+
+			// Steps will never be equal because DeepEqual compares addresses.
+			var gotSteps, wantSteps []*types.Step
+			gotSteps, codelab.Steps = codelab.Steps, []*types.Step{}
+			wantSteps, tc.out.codelab.Steps = tc.out.codelab.Steps, []*types.Step{}
+
+			if tc.ok {
+				if !reflect.DeepEqual(codelab, tc.out.codelab) || !reflect.DeepEqual(metadata, tc.out.metadata) {
+					t.Errorf("Parse(string) = %#v, %+v, want %#v, %+v", codelab, metadata, tc.out.codelab, tc.out.metadata)
+					return
+				}
+
+				if len(gotSteps) != len(wantSteps) {
+					t.Errorf("Parse(string) got %d steps, want %d", len(gotSteps), len(wantSteps))
+					return
+				}
+				for i, v := range gotSteps {
+					// We don't care about content yet.
+					// TODO pay attention to content equality
+					v.Content = nil
+					if !reflect.DeepEqual(*v, *wantSteps[i]) {
+						t.Errorf("Parse(string).codelab.steps[%d] = %+v, want %+v", i, *v, *wantSteps[i])
+					}
+				}
+			}
+		})
+	}
+}
 
 func TestSeek(t *testing.T) {
 	testData := []byte(`<body>
