7b4b188c3ad5139e618549be3885191076b43fcb
marcacohen <marcacohen@gmail.com>
2019-12-08 08:58:31 +0000

    fix pdf merging

--- clprint.js
+++ clprint.js
@@ -4,26 +4,29 @@ const merge = require('easy-pdf-merge');
 const puppeteer = require('puppeteer');
 const url = 'https://codelabs.developers.google.com/codelabs/'
 
+var files=[];
+
+const exit = () => {files.forEach((f) => {fs.unlinkSync(f)})};
+
 const get = async (lab, first, last) => {
-  const browser = await puppeteer.launch();
-  var pdfFiles=[];
+  let browser = await puppeteer.launch();
   for (i = first; i <= last; i++) {
     console.log('fetching step ' + i);
-    const page = await browser.newPage();
-    const file = lab + '.' + i + '.pdf';
-    await page.goto(url+lab+'/index.html#' + i, {waitUntil: 'networkidle0'});
+    let page = await browser.newPage();
+    let file = lab + '.' + i + '.pdf';
+    await page.goto(url+lab+'/index.html#'+i, {waitUntil: 'networkidle0'});
     let height = await page.evaluate(i => {return document.getElementsByTagName('google-codelab-step')[i].scrollHeight}, i);
     await page.pdf({path: file, height: height});
-    pdfFiles.push(file);
+    files.push(file);
   }
-  if (pdfFiles.length > 1) {
-    await merge(pdfFiles, lab+'.pdf', (err)=>{
+  await browser.close();
+  if (files.length > 1) {
+    merge(files, lab+'.pdf', (err)=>{
       if (err) return console.log(err);
       else console.log('Successfully merged!');
     });
-    pdfFiles.forEach((file) => {fs.unlinkSync(file)});
   }
-  await browser.close();
 };
 
+process.on('exit', exit);
 get(process.argv[2], process.argv[3], process.argv[4]);
