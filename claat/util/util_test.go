8dceb15dcd0ce6caaa7cf4ef94bcd187ae4dc4b1
Michael Richardson <mrichar@google.com>
2021-07-08 11:09:16 -0500

    Strip spaces from keywords and normalize keyword parsing (#600)
    * Update parse.go
    * Update util.go
    * Update util_test.go

--- claat/util/util_test.go
+++ claat/util/util_test.go
@@ -37,3 +37,85 @@ func TestUnique(t *testing.T) {
 		})
 	}
 }
+
+func TestNormalizedSplit(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		out  []string
+	}{
+		{
+			name: "none",
+			in:   "",
+			out:  []string{},
+		},
+		{
+			name: "one",
+			in:   "peach",
+			out:  []string{"peach"},
+		},
+		{
+			name: "whitespace",
+			in:   "   ",
+			out:  []string{},
+		},
+		{
+			name: "commas",
+			in:   ",,,",
+			out:  []string{},
+		},
+		{
+			name: "split",
+			in:   "peach,pear",
+			out:  []string{"peach", "pear"},
+		},
+		{
+			name: "split and trim space",
+			in:   "peach , pear",
+			out:  []string{"peach", "pear"},
+		},
+		{
+			name: "split and trim consequtive spaces",
+			in:   "peach  ,  pear",
+			out:  []string{"peach", "pear"},
+		},
+		{
+			name: "split and collapse space",
+			in:   "p e a c h,pear",
+			out:  []string{"peach", "pear"},
+		},
+		{
+			name: "split and remove duplicates",
+			in:   "peach,pear,pear",
+			out:  []string{"peach", "pear"},
+		},
+		{
+			name: "split and lowercase",
+			in:   "PEACH,pear",
+			out:  []string{"peach", "pear"},
+		},
+		{
+			name: "split and strip new lines",
+			in:   "pea\nch,pear\n",
+			out:  []string{"peach", "pear"},
+		},
+		{
+			name: "split and strip tabs",
+			in:   "pea\tch,pear\t",
+			out:  []string{"peach", "pear"},
+		},
+		{
+			name: "split and strip whitespace",
+			in:   "pea\tch, pear\n",
+			out:  []string{"peach", "pear"},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			out := NormalizedSplit(tc.in)
+			if diff := cmp.Diff(tc.out, out); diff != "" {
+				t.Errorf("NormalizedSplit(%+v) got diff (-want, +got)=\n%s", tc.in, diff)
+			}
+		})
+	}
+}
