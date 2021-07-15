cb3ea6f223da802d518f1d4eeca13614ea5b4d6e
Cassie Recher <cerech@google.com>
2021-07-15 22:22:34 +0000

    Add tests for MD parsing isItalic.

--- claat/parser/md/html_test.go
+++ claat/parser/md/html_test.go
@@ -158,3 +158,155 @@ func TestIsBold(t *testing.T) {
 		})
 	}
 }
+
+func TestIsItalic(t *testing.T) {
+	// <em>foobar</em>
+	a1 := &html.Node{
+		Type:     html.ElementNode,
+		DataAtom: atom.Em,
+		Data:     "em",
+	}
+	a2 := &html.Node{
+		Type: html.TextNode,
+		Data: "foobar",
+	}
+	a1.AppendChild(a2)
+
+	// <i>foobar</i>
+	b1 := &html.Node{
+		Type:     html.ElementNode,
+		DataAtom: atom.I,
+		Data:     "i",
+	}
+	b2 := &html.Node{
+		Type: html.TextNode,
+		Data: "foobar",
+	}
+	b1.AppendChild(b2)
+
+	// <em><code>foobar</code></em>
+	c1 := &html.Node{
+		Type:     html.ElementNode,
+		DataAtom: atom.Em,
+		Data:     "em",
+	}
+	c2 := &html.Node{
+		Type:     html.ElementNode,
+		DataAtom: atom.Code,
+		Data:     "code",
+	}
+	c3 := &html.Node{
+		Type: html.TextNode,
+		Data: "foobar",
+	}
+	c1.AppendChild(c2)
+	c2.AppendChild(c3)
+
+	// <i><code>foobar</code></i>
+	d1 := &html.Node{
+		Type:     html.ElementNode,
+		DataAtom: atom.I,
+		Data:     "i",
+	}
+	d2 := &html.Node{
+		Type:     html.ElementNode,
+		DataAtom: atom.Code,
+		Data:     "code",
+	}
+	d3 := &html.Node{
+		Type: html.TextNode,
+		Data: "foobar",
+	}
+	d1.AppendChild(d2)
+	d2.AppendChild(d3)
+
+	// <p>foobar</p>
+	e1 := &html.Node{
+		Type:     html.ElementNode,
+		DataAtom: atom.P,
+		Data:     "p",
+	}
+	e2 := &html.Node{
+		Type: html.TextNode,
+		Data: "foobar",
+	}
+	e1.AppendChild(e2)
+
+	tests := []struct {
+		name string
+		in   *html.Node
+		out  bool
+	}{
+		{
+			name: "EmText_Em",
+			in:   a1,
+			out:  true,
+		},
+		{
+			name: "EmText_Em",
+			in:   a2,
+			out:  true,
+		},
+		{
+			name: "IText_I",
+			in:   b1,
+			out:  true,
+		},
+		{
+			name: "IText_Text",
+			in:   b2,
+			out:  true,
+		},
+		{
+			name: "EmCodeText_Em",
+			in:   c1,
+			out:  true,
+		},
+		{
+			name: "EmCodeText_Code",
+			in:   c2,
+			out:  true,
+		},
+		/*
+			// TODO: I think this should work but it doesn't.
+			{
+				name: "EmCodeText_Text",
+				in:   c3,
+				out:  true,
+			},
+		*/
+		{
+			name: "ICodeText_I",
+			in:   d1,
+			out:  true,
+		},
+		{
+			name: "ICodeText_Code",
+			in:   d2,
+			out:  true,
+		},
+		/*
+			// TODO: I think this should work but it doesn't
+			{
+				name: "ICodeText_Text",
+				in:   d3,
+				out:  true,
+			},
+		*/
+		{
+			name: "PText_P",
+			in:   e1,
+		},
+		{
+			name: "PText_Text",
+			in:   e2,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if out := isItalic(tc.in); out != tc.out {
+				t.Errorf("isItalic(%v) = %t, want %t", tc.in, out, tc.out)
+			}
+		})
+	}
+}
