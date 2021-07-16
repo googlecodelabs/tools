49cb41c3e31045fb92101752d50631c905ef19ad
Cassie Recher <cerech@google.com>
2021-07-16 20:29:50 +0000

    Change makeTextNode to have the text itself be configurable.

--- claat/parser/md/html_test.go
+++ claat/parser/md/html_test.go
@@ -42,10 +42,11 @@ func makeINode() *html.Node {
 	}
 }
 
-func makeTextNode() *html.Node {
+// data is the text in the node.
+func makeTextNode(data string) *html.Node {
 	return &html.Node{
 		Type: html.TextNode,
-		Data: "foobar",
+		Data: data,
 	}
 }
 
@@ -166,36 +167,36 @@ func TestIsHeader(t *testing.T) {
 func TestIsBold(t *testing.T) {
 	// <strong>foobar</strong>
 	a1 := makeStrongNode()
-	a2 := makeTextNode()
+	a2 := makeTextNode("foobar")
 	a1.AppendChild(a2)
 
 	// <b>foobar</b>
 	b1 := makeBNode()
-	b2 := makeTextNode()
+	b2 := makeTextNode("foobar")
 	b1.AppendChild(b2)
 
 	// <strong><code>foobar</code></strong>
 	c1 := makeStrongNode()
 	c2 := makeCodeNode()
-	c3 := makeTextNode()
+	c3 := makeTextNode("foobar")
 	c1.AppendChild(c2)
 	c2.AppendChild(c3)
 
 	// <b><code>foobar</code></b>
 	d1 := makeBNode()
 	d2 := makeCodeNode()
-	d3 := makeTextNode()
+	d3 := makeTextNode("foobar")
 	d1.AppendChild(d2)
 	d2.AppendChild(d3)
 
 	// <p>foobar</p>
 	e1 := makePNode()
-	e2 := makeTextNode()
+	e2 := makeTextNode("foobar")
 	e1.AppendChild(e2)
 
 	// <i>foobar</i>
 	f1 := makeINode()
-	f2 := makeTextNode()
+	f2 := makeTextNode("foobar")
 	f1.AppendChild(f2)
 
 	tests := []struct {
@@ -288,36 +289,36 @@ func TestIsBold(t *testing.T) {
 func TestIsItalic(t *testing.T) {
 	// <em>foobar</em>
 	a1 := makeEmNode()
-	a2 := makeTextNode()
+	a2 := makeTextNode("foobar")
 	a1.AppendChild(a2)
 
 	// <i>foobar</i>
 	b1 := makeINode()
-	b2 := makeTextNode()
+	b2 := makeTextNode("foobar")
 	b1.AppendChild(b2)
 
 	// <em><code>foobar</code></em>
 	c1 := makeEmNode()
 	c2 := makeCodeNode()
-	c3 := makeTextNode()
+	c3 := makeTextNode("foobar")
 	c1.AppendChild(c2)
 	c2.AppendChild(c3)
 
 	// <i><code>foobar</code></i>
 	d1 := makeINode()
 	d2 := makeCodeNode()
-	d3 := makeTextNode()
+	d3 := makeTextNode("foobar")
 	d1.AppendChild(d2)
 	d2.AppendChild(d3)
 
 	// <p>foobar</p>
 	e1 := makePNode()
-	e2 := makeTextNode()
+	e2 := makeTextNode("foobar")
 	e1.AppendChild(e2)
 
 	// <b>foobar</b>
 	f1 := makeBNode()
-	f2 := makeTextNode()
+	f2 := makeTextNode("foobar")
 	f1.AppendChild(f2)
 
 	tests := []struct {
@@ -411,28 +412,28 @@ func TestIsBoldAndItalic(t *testing.T) {
 	// <em><strong>foobar</strong></em>
 	a1 := makeEmNode()
 	a2 := makeStrongNode()
-	a3 := makeTextNode()
+	a3 := makeTextNode("foobar")
 	a1.AppendChild(a2)
 	a2.AppendChild(a3)
 
 	// <i><strong>foobar</strong></i>
 	b1 := makeINode()
 	b2 := makeStrongNode()
-	b3 := makeTextNode()
+	b3 := makeTextNode("foobar")
 	b1.AppendChild(b2)
 	b2.AppendChild(b3)
 
 	// <em><b>foobar</b></em>
 	c1 := makeEmNode()
 	c2 := makeBNode()
-	c3 := makeTextNode()
+	c3 := makeTextNode("foobar")
 	c1.AppendChild(c2)
 	c2.AppendChild(c3)
 
 	// <i><b>foobar</b></i>
 	d1 := makeINode()
 	d2 := makeBNode()
-	d3 := makeTextNode()
+	d3 := makeTextNode("foobar")
 	d1.AppendChild(d2)
 	d2.AppendChild(d3)
 
@@ -440,7 +441,7 @@ func TestIsBoldAndItalic(t *testing.T) {
 	e1 := makeEmNode()
 	e2 := makeStrongNode()
 	e3 := makeCodeNode()
-	e4 := makeTextNode()
+	e4 := makeTextNode("foobar")
 	e1.AppendChild(e2)
 	e2.AppendChild(e3)
 	e3.AppendChild(e4)
@@ -449,7 +450,7 @@ func TestIsBoldAndItalic(t *testing.T) {
 	f1 := makeEmNode()
 	f2 := makeCodeNode()
 	f3 := makeStrongNode()
-	f4 := makeTextNode()
+	f4 := makeTextNode("foobar")
 	f1.AppendChild(f2)
 	f2.AppendChild(f3)
 	f3.AppendChild(f4)
@@ -457,14 +458,14 @@ func TestIsBoldAndItalic(t *testing.T) {
 	// <strong><em>foobar</em></strong>
 	g1 := makeStrongNode()
 	g2 := makeEmNode()
-	g3 := makeTextNode()
+	g3 := makeTextNode("foobar")
 	g1.AppendChild(g2)
 	g2.AppendChild(g3)
 
 	// <strong><i>foobar</i></strong>
 	h1 := makeStrongNode()
 	h2 := makeINode()
-	h3 := makeTextNode()
+	h3 := makeTextNode("foobar")
 	h1.AppendChild(h2)
 	h2.AppendChild(h3)
 
@@ -472,14 +473,14 @@ func TestIsBoldAndItalic(t *testing.T) {
 	// Skipped i and j due to widespread use of <i>
 	k1 := makeBNode()
 	k2 := makeEmNode()
-	k3 := makeTextNode()
+	k3 := makeTextNode("foobar")
 	k1.AppendChild(k2)
 	k2.AppendChild(k3)
 
 	// <b><i>foobar</i></b>
 	l1 := makeBNode()
 	l2 := makeINode()
-	l3 := makeTextNode()
+	l3 := makeTextNode("foobar")
 	l1.AppendChild(l2)
 	l2.AppendChild(l3)
 
@@ -487,7 +488,7 @@ func TestIsBoldAndItalic(t *testing.T) {
 	m1 := makeStrongNode()
 	m2 := makeEmNode()
 	m3 := makeCodeNode()
-	m4 := makeTextNode()
+	m4 := makeTextNode("foobar")
 	m1.AppendChild(m2)
 	m2.AppendChild(m3)
 	m3.AppendChild(m4)
@@ -496,24 +497,24 @@ func TestIsBoldAndItalic(t *testing.T) {
 	n1 := makeStrongNode()
 	n2 := makeCodeNode()
 	n3 := makeEmNode()
-	n4 := makeTextNode()
+	n4 := makeTextNode("foobar")
 	n1.AppendChild(n2)
 	n2.AppendChild(n3)
 	n3.AppendChild(n4)
 
 	// <p>foobar</p>
 	o1 := makePNode()
-	o2 := makeTextNode()
+	o2 := makeTextNode("foobar")
 	o1.AppendChild(o2)
 
 	// <em>foobar</em>
 	p1 := makeEmNode()
-	p2 := makeTextNode()
+	p2 := makeTextNode("foobar")
 	p1.AppendChild(p2)
 
 	// <strong>foobar</strong>
 	q1 := makeStrongNode()
-	q2 := makeTextNode()
+	q2 := makeTextNode("foobar")
 	q1.AppendChild(q2)
 
 	tests := []struct {
@@ -736,23 +737,23 @@ func TestIsConsole(t *testing.T) {
 	// <code class="language-console">foobar</code>
 	a1 := makeCodeNode()
 	a1.Attr = append(a1.Attr, html.Attribute{Key: "class", Val: "language-console"})
-	a2 := makeTextNode()
+	a2 := makeTextNode("foobar")
 	a1.AppendChild(a2)
 
 	// <code class="language-js">foobar</code>
 	b1 := makeCodeNode()
 	b1.Attr = append(b1.Attr, html.Attribute{Key: "class", Val: "language-js"})
-	b2 := makeTextNode()
+	b2 := makeTextNode("foobar")
 	b1.AppendChild(b2)
 
 	// <code>foobar</code>
 	c1 := makeCodeNode()
-	c2 := makeTextNode()
+	c2 := makeTextNode("foobar")
 	c1.AppendChild(c2)
 
 	// <p>foobar</p>
 	d1 := makePNode()
-	d2 := makeTextNode()
+	d2 := makeTextNode("foobar")
 	d1.AppendChild(d2)
 
 	tests := []struct {
@@ -808,23 +809,23 @@ func TestIsCode(t *testing.T) {
 	// <code class="language-console">foobar</code>
 	a1 := makeCodeNode()
 	a1.Attr = append(a1.Attr, html.Attribute{Key: "class", Val: "language-console"})
-	a2 := makeTextNode()
+	a2 := makeTextNode("foobar")
 	a1.AppendChild(a2)
 
 	// <code class="language-js">foobar</code>
 	b1 := makeCodeNode()
 	b1.Attr = append(b1.Attr, html.Attribute{Key: "class", Val: "language-js"})
-	b2 := makeTextNode()
+	b2 := makeTextNode("foobar")
 	b1.AppendChild(b2)
 
 	// <code>foobar</code>
 	c1 := makeCodeNode()
-	c2 := makeTextNode()
+	c2 := makeTextNode("foobar")
 	c1.AppendChild(c2)
 
 	// <p>foobar</p>
 	d1 := makePNode()
-	d2 := makeTextNode()
+	d2 := makeTextNode("foobar")
 	d1.AppendChild(d2)
 
 	tests := []struct {
@@ -880,7 +881,7 @@ func TestIsCode(t *testing.T) {
 
 func TestIsButton(t *testing.T) {
 	a1 := makeButtonNode()
-	a2 := makeTextNode()
+	a2 := makeTextNode("foobar")
 	a1.AppendChild(a2)
 
 	tests := []struct {
@@ -918,7 +919,7 @@ func TestIsButton(t *testing.T) {
 
 func TestIsAside(t *testing.T) {
 	a1 := makeAsideNode()
-	a2 := makeTextNode()
+	a2 := makeTextNode("foobar")
 	a1.AppendChild(a2)
 
 	tests := []struct {
