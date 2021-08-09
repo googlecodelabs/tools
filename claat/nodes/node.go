06b1207579ec8874aa8aaa211bdf635c3f6fcee2
Cassie Recher <cerech@google.com>
2021-08-09 23:19:10 +0000

    Add tests for URLNode.

--- claat/nodes/node.go
+++ claat/nodes/node.go
@@ -345,27 +345,3 @@ type HeaderNode struct {
 func (hn *HeaderNode) Empty() bool {
 	return hn.Content.Empty()
 }
-
-// NewURLNode creates a new Node of type NodeURL with optinal content n.
-func NewURLNode(url string, n ...Node) *URLNode {
-	return &URLNode{
-		node:    node{typ: NodeURL},
-		URL:     url,
-		Target:  "_blank",
-		Content: NewListNode(n...),
-	}
-}
-
-// URLNode represents elements such as <a href="...">
-type URLNode struct {
-	node
-	URL     string
-	Name    string
-	Target  string
-	Content *ListNode
-}
-
-// Empty returns true if un content is empty.
-func (un *URLNode) Empty() bool {
-	return un.Content.Empty()
-}
---
+++ claat/nodes/url_test.go
@@ -0,0 +1,83 @@
+package nodes
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// AllowUnexported option for cmp to make sure we can diff properly.
+var cmpOptURL = cmp.AllowUnexported(URLNode{}, node{}, ListNode{}, TextNode{})
+
+func TestNewURLNode(t *testing.T) {
+	tests := []struct {
+		name      string
+		inURL     string
+		inContent []Node
+		out       *URLNode
+	}{
+		{
+			name: "Empty",
+			out: &URLNode{
+				node:    node{typ: NodeURL},
+				Target:  "_blank",
+				Content: NewListNode(),
+			},
+		},
+		{
+			name:  "NonEmpty",
+			inURL: "google.com",
+			inContent: []Node{
+				NewTextNode("foo"),
+				NewTextNode("bar"),
+			},
+			out: &URLNode{
+				node:    node{typ: NodeURL},
+				URL:     "google.com",
+				Target:  "_blank",
+				Content: NewListNode(NewTextNode("foo"), NewTextNode("bar")),
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			out := NewURLNode(tc.inURL, tc.inContent...)
+			if diff := cmp.Diff(tc.out, out, cmpOptURL); diff != "" {
+				t.Errorf("NewURLNode(%s, %+v) got diff (-want +got): %s", tc.inURL, tc.inContent, diff)
+				return
+			}
+		})
+	}
+}
+
+func TestURLNodeEmpty(t *testing.T) {
+	tests := []struct {
+		name      string
+		inURL     string
+		inContent []Node
+		out       bool
+	}{
+		{
+			name: "Empty",
+			out:  true,
+		},
+		{
+			name:  "NonEmpty",
+			inURL: "google.com",
+			inContent: []Node{
+				NewTextNode("foo"),
+				NewTextNode("bar"),
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			n := NewURLNode(tc.inURL, tc.inContent...)
+			out := n.Empty()
+			if out != tc.out {
+				t.Errorf("ButtonNode.Empty() = %t, want %t", out, tc.out)
+				return
+			}
+		})
+	}
+}
