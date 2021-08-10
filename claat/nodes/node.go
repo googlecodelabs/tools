ef101a4df83c27a762d2ee3cdcc96ff24a9c235a
Cassie Recher <cerech@google.com>
2021-08-10 18:36:34 +0000

    Add tests for CodeNode.

--- claat/nodes/node.go
+++ claat/nodes/node.go
@@ -300,27 +300,3 @@ type TextNode struct {
 func (tn *TextNode) Empty() bool {
 	return strings.TrimSpace(tn.Value) == ""
 }
-
-// NewCodeNode creates a new Node of type NodeCode.
-// Use term argument to specify a terminal output.
-func NewCodeNode(v string, term bool, lang string) *CodeNode {
-	return &CodeNode{
-		node:  node{typ: NodeCode},
-		Value: v,
-		Term:  term,
-		Lang:  lang,
-	}
-}
-
-// CodeNode is either a source code snippet or a terminal output.
-type CodeNode struct {
-	node
-	Term  bool
-	Lang  string
-	Value string
-}
-
-// Empty returns true if cn.Value is zero, exluding space runes.
-func (cn *CodeNode) Empty() bool {
-	return strings.TrimSpace(cn.Value) == ""
-}
