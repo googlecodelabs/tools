16a3eef50ff6b601324c7e1945d6b1b7cbf0227c
Cassie Recher <cerech@google.com>
2021-08-11 18:30:52 +0000

    Add tests for GridNode.

--- claat/nodes/node.go
+++ claat/nodes/node.go
@@ -204,39 +204,6 @@ func ImportNodes(nodes []Node) []*ImportNode {
 	return imps
 }
 
-// NewGridNode creates a new grid with optional content.
-func NewGridNode(rows ...[]*GridCell) *GridNode {
-	return &GridNode{
-		node: node{typ: NodeGrid},
-		Rows: rows,
-	}
-}
-
-// GridNode is a 2d matrix.
-type GridNode struct {
-	node
-	Rows [][]*GridCell
-}
-
-// GridCell is a cell of GridNode.
-type GridCell struct {
-	Colspan int
-	Rowspan int
-	Content *ListNode
-}
-
-// Empty returns true when every cell has empty content.
-func (gn *GridNode) Empty() bool {
-	for _, r := range gn.Rows {
-		for _, c := range r {
-			if !c.Content.Empty() {
-				return false
-			}
-		}
-	}
-	return true
-}
-
 // NewItemsListNode creates a new ItemsListNode of type NodeItemsList,
 // which defaults to an unordered list.
 // Provide a positive start to make this a numbered list.
