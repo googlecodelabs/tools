eec2501b4cbe725c9088df5a5304e6e0806c7f3b
Cassie Recher <cerech@google.com>
2021-08-17 22:17:03 +0000

    Add comment to HeaderNode's MutateType.

--- claat/nodes/header.go
+++ claat/nodes/header.go
@@ -26,6 +26,7 @@ func IsHeader(t NodeType) bool {
 	return t&(NodeHeader|NodeHeaderCheck|NodeHeaderFAQ) != 0
 }
 
+// MutateType sets the header's node type if the given type is a header type.
 func (hn *HeaderNode) MutateType(t NodeType) {
 	if IsHeader(t) {
 		hn.typ = t
