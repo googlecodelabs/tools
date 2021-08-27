ce19864a450fcbb4bd5de699f328ffc92998f12a
Cassie Recher <cerech@google.com>
2021-08-27 17:26:22 +0000

    Add tests for gdoc findChildAtoms.

--- claat/parser/gdoc/html_test.go
+++ claat/parser/gdoc/html_test.go
@@ -312,7 +312,83 @@ func TestFindAtom(t *testing.T) {
 	}
 }
 
-// TODO: test findChildAtoms
+func TestFindChildAtoms(t *testing.T) {
+	a1 := makePNode()
+	a2 := makeEmNode()
+	a3 := makeTextNode("foobar")
+	a1.AppendChild(a2)
+	a2.AppendChild(a3)
+
+	b1 := makePNode()
+	b2 := makeCodeNode()
+	b3 := makeEmNode()
+	b4 := makeStrongNode()
+	b5 := makeTextNode("foobar")
+	b1.AppendChild(b2)
+	b2.AppendChild(b3)
+	b3.AppendChild(b4)
+	b4.AppendChild(b5)
+
+	c1 := makePNode()
+	c2 := makeCodeNode()
+	c3 := makeTextNode("foobar1")
+	c4 := makeEmNode()
+	c5 := makeTextNode("foobar2")
+	c6 := makeStrongNode()
+	c7 := makeCodeNode()
+	c8 := makeTextNode("foobar3")
+	//<p><code>foobar1</code><em>foobar2</em><strong><code>foobar3</code></strong></p>
+	c1.AppendChild(c2)
+	c2.AppendChild(c3)
+	c1.AppendChild(c4)
+	c4.AppendChild(c5)
+	c1.AppendChild(c6)
+	c6.AppendChild(c7)
+	c7.AppendChild(c8)
+
+	tests := []struct {
+		name   string
+		inNode *html.Node
+		inAtom atom.Atom
+		out    []*html.Node
+	}{
+		{
+			name:   "One",
+			inNode: a1,
+			inAtom: atom.Em,
+			out:    []*html.Node{a2},
+		},
+		{
+			name:   "DistantDescendant",
+			inNode: b1,
+			inAtom: atom.Strong,
+			out:    []*html.Node{b4},
+		},
+		{
+			name:   "Multi",
+			inNode: c1,
+			inAtom: atom.Code,
+			out:    []*html.Node{c2, c7},
+		},
+		{
+			name:   "None",
+			inNode: a1,
+			inAtom: atom.Marquee,
+		},
+		{
+			name:   "Self",
+			inNode: a1,
+			inAtom: atom.P,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if diff := cmp.Diff(tc.out, findChildAtoms(tc.inNode, tc.inAtom)); diff != "" {
+				t.Errorf("findChildAtoms(%+v, %+v) got diff (-want +got):\n%s", tc.inNode, tc.inAtom, diff)
+			}
+		})
+	}
+}
 
 func TestFindParent(t *testing.T) {
 	a1 := makePNode()
