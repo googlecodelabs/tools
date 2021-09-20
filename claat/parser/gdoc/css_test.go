f76fbd4d3817bbb0d80709cec784e13733232cd8
Cassie Recher <cerech@google.com>
2021-09-20 21:33:12 +0000

    Create css_test.go and add TODOs to it.

---
+++ claat/parser/gdoc/css_test.go
@@ -0,0 +1,13 @@
+package gdoc
+
+// TODO: test parseStyle
+
+// TODO: test classList
+
+// TODO: test hasClass
+
+// TODO: test hasClassStyle
+
+// TODO: test styleValue
+
+// TODO: test styleFloatValue
