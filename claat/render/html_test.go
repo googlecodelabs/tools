3c747de7fe31c132ff4611ae298d8954a0133071
Cassie Recher <cerech@google.com>
2021-09-24 23:26:06 +0000

    Add tests for HTML survey rendering.

--- claat/render/html_test.go
+++ claat/render/html_test.go
@@ -73,7 +73,111 @@ func TestHTMLEnv(t *testing.T) {
 // TODO: test itemsList
 // TODO: test grid
 // TODO: test infobox
-// TODO: test survey
+
+func TestSurvey(t *testing.T) {
+	tests := []struct {
+		name   string
+		inNode *nodes.SurveyNode
+		out    string
+	}{
+		{
+			name: "NoID",
+			inNode: nodes.NewSurveyNode("",
+				&nodes.SurveyGroup{
+					Name:    "pick a number",
+					Options: []string{"1", "2", "3"},
+				},
+				&nodes.SurveyGroup{
+					Name:    "choose an answer",
+					Options: []string{"yes", "no", "probably"},
+				},
+			),
+			out: `<google-codelab-survey survey-id="">
+<h4>pick a number</h4>
+<paper-radio-group>
+<paper-radio-button>1</paper-radio-button>
+<paper-radio-button>2</paper-radio-button>
+<paper-radio-button>3</paper-radio-button>
+</paper-radio-group>
+<h4>choose an answer</h4>
+<paper-radio-group>
+<paper-radio-button>yes</paper-radio-button>
+<paper-radio-button>no</paper-radio-button>
+<paper-radio-button>probably</paper-radio-button>
+</paper-radio-group>
+</google-codelab-survey>`,
+		},
+		{
+			name:   "NoGroups",
+			inNode: nodes.NewSurveyNode("foobar"),
+			out: `<google-codelab-survey survey-id="foobar">
+</google-codelab-survey>`,
+		},
+		{
+			name: "Simple",
+			inNode: nodes.NewSurveyNode("a simple example",
+				&nodes.SurveyGroup{
+					Name:    "pick a color",
+					Options: []string{"red", "blue", "yellow"},
+				}),
+			out: `<google-codelab-survey survey-id="a simple example">
+<h4>pick a color</h4>
+<paper-radio-group>
+<paper-radio-button>red</paper-radio-button>
+<paper-radio-button>blue</paper-radio-button>
+<paper-radio-button>yellow</paper-radio-button>
+</paper-radio-group>
+</google-codelab-survey>`,
+		},
+		{
+			name: "MultipleGroups",
+			inNode: nodes.NewSurveyNode("an example with multiple survey groups",
+				&nodes.SurveyGroup{
+					Name:    "a",
+					Options: []string{"a", "aa", "aaa"},
+				},
+				&nodes.SurveyGroup{
+					Name:    "b",
+					Options: []string{"b", "bb", "bbb"},
+				},
+				&nodes.SurveyGroup{
+					Name:    "c",
+					Options: []string{"c", "cc", "ccc"},
+				}),
+			out: `<google-codelab-survey survey-id="an example with multiple survey groups">
+<h4>a</h4>
+<paper-radio-group>
+<paper-radio-button>a</paper-radio-button>
+<paper-radio-button>aa</paper-radio-button>
+<paper-radio-button>aaa</paper-radio-button>
+</paper-radio-group>
+<h4>b</h4>
+<paper-radio-group>
+<paper-radio-button>b</paper-radio-button>
+<paper-radio-button>bb</paper-radio-button>
+<paper-radio-button>bbb</paper-radio-button>
+</paper-radio-group>
+<h4>c</h4>
+<paper-radio-group>
+<paper-radio-button>c</paper-radio-button>
+<paper-radio-button>cc</paper-radio-button>
+<paper-radio-button>ccc</paper-radio-button>
+</paper-radio-group>
+</google-codelab-survey>`,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			outBuffer := &bytes.Buffer{}
+			hw := &htmlWriter{w: outBuffer}
+			hw.survey(tc.inNode)
+			out := outBuffer.String()
+			if diff := cmp.Diff(tc.out, out); diff != "" {
+				t.Errorf("hw.survey(%+v) got diff (-want +got):\n%s", tc.inNode, diff)
+			}
+		})
+	}
+}
 
 func TestHeader(t *testing.T) {
 	a1 := nodes.NewTextNode("foo")
